4f78c1f0f0e9fcdb9ca5f8cf1fb1421beb491434
rahul rao <rahul.seth.rao@gmail.com>
2022-06-02 12:42:16 -0500

    added some notes for problems 1 and 2

--- problem2.py
+++ problem2.py
@@ -14,7 +14,7 @@ DATASET_FILE_NAME = 'test2.csv'
 df = pd.read_csv(DATASET_FILE_NAME)
 itemsets_1 = {item:0 for item in df}
 
-# 1 itemsets above the min sup threshold
+# 1-itemsets above the min sup threshold
 for i in df:
     for j in df[i]:
         if j == i:
@@ -30,9 +30,42 @@ for i in range(len(keys_list)):
     for j in range(i+1, len(keys_list)):
         itemsets_2[(keys_list[i], keys_list[j])] = 0
 
+### TODO
+# iterate over rows of df
+# for each row check all of the 2-tuples in 'itemsets_2'
+# see if each one is in that row of df
+# if it is, then update the corresponding value in 'itemsets_2' by incrementing 'itemsets_2[i]' by 1
+# (for whichever 'i' it is)
+
+
 
 # ************************
 # STEP 2
+
+### TODO
+# (sidenote) store graph data as an adjacency list (as opposed to an adjacency matrix)
+# (continuing from above) example:      
+#                3
+#           A ------- B
+#            \       /
+#          2  \     / 5
+#              \   /
+#               \ /     1
+#                C ----------- D
+# the above graph would be represented as such:
+# {
+#    A: [(B, 3), (C, 2)],
+#    B: [(A, 3), (C, 5)],
+#    C: [(A, 2), (B, 5), (D, 1)],
+#    D: [(C, 1)]
+# }
+# iterate over the keys from step 1
+# (we only care about the 2-itemsets (we created the 1-itemsets only to help with creating the 2-itemsets))
+# (each element of 'itemsets_2' is of the form (a, b): w, where a and b are 2 distinct items and w is the corresponding weight)
+# for element of 'itemsets_2', add 2 things to the adjacency list representation of the IAG:
+# 1. a: [(b, w)]
+# 2. b: [(a, w)]
+
 
 # ************************
 # STEP 3
