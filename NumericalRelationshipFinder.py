f0b9e92382551918ce54ad946ade7184fb9cf208
Henry Xie <68752252+hlxie22@users.noreply.github.com>
2022-07-06 21:08:39 +0000

    Almost completed numerical relationship finder

---
+++ NumericalRelationshipFinder.py
@@ -0,0 +1,90 @@
+# This will search for linear relationships between numerical values. 
+
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import Lasso
+
+#################################
+# GLOBAL CONSTS
+
+FILE_NAME = 'testWithPaperData.csv'
+CORR_THRESHOLD = 0.7
+MODEL_SCORE_THRESHOLD = 0.7
+
+df_num = df.select_dtypes(include=[np.number])
+df_corr = df_num.corr()
+
+# Create the adjacency matrix
+
+adjacency_matrix = {}
+
+for col in df_corr.columns:
+  adj = []
+  for i in range(len(df_corr[col].size)):
+    corr = df_corr[col][i]
+    if abs(corr) > CORR_THRESHOLD and corr != 1:
+      adj.append(df_corr.columns[i])
+  if len(adj) > 0:
+    adjacency_matrix[col] = adj
+
+# Partition the adjacency matrix into disjoint connected components
+# "connected_components" is a list of lists. Each list within "connected_components" contains the items in each connected component of the graph
+# Based off the algorithm presented here: https://stackoverflow.com/questions/8124626/finding-connected-components-of-adjacency-matrix-graph
+
+num_components = 0
+connected_components = []
+groupings = {}
+
+for key in adjacency_matrix:
+  groupings[key] = 0
+
+for key in adjacency_matrix:
+  if groupings[key] == 0:
+    num_components++
+    component = []
+    queue = [key]
+    while len(queue) > 0:
+      next_item = queue.pop(0)
+      for item in adjacency_matrix[next_item]:
+        if groupings[item] == 0:
+          queue.append(item)
+      groupings[next_item] = num_components
+      component.append(next_item)
+    connected_components.append(component)
+
+# Train linear regression on each connected component (use L1 regularization)
+# Each value in model_scores is the R^2 value
+# Throw out relationships that are relatively weak, based on R^2 value
+
+
+model_weights = []
+model_intercepts = []
+model_scores = []
+
+zeroes = np.array([0])
+zeroes = np.repeat(zeroes, df.shape[0])
+
+lasso = Lasso()
+
+for component in connected_components:
+  X_train = df[component]
+  y_train = zeroes
+  lasso.fit(X_train, y_train)
+  if lasso.score() > MODEL_SCORE_THRESHOLD:
+    model_weights.append(lasso.coef_)
+    model_intercepts.append(lasso.intercept_)
+    model_scores.append(lasso.score())
+
+num_models = len(connected_components)
+
+    
+# Round the coefficients, because most relationships will likely be simple ones involving integer coefficients
+
+for i in range(num_models):
+  relationship = 
+  output = "Numerical Relationship #{index}: {relationship}"
+  print(output.format(index = i, relationship = relationship))
+  
+
+# Store final relationships
+
