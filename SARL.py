64488b683979403f77b234e2194921da73940d53
rahul rao <rahul.seth.rao@gmail.com>
2022-07-13 17:32:32 -0500

    cleaned up code (still works)

--- SARL.py
+++ SARL.py
@@ -7,10 +7,8 @@ from mlxtend.frequent_patterns import fpgrowth
 from mlxtend.frequent_patterns import apriori
 from mlxtend.preprocessing import TransactionEncoder
 from mlxtend.frequent_patterns import association_rules
-import networkx as nx
-import metis
 import pymetis
-import time
+
 
 #################################
 # GLOBAL CONSTS
@@ -19,7 +17,6 @@ FILE_NAME = 'testWithPaperData.csv'
 MIN_SUP = 0.1
 MIN_CONF = 0.7
 NUM_PARTS = 2 # k for MLkP
-NUM_CUTS = 2 # METIS specific param
 
 #################################
 # SARL CLASS
@@ -29,18 +26,12 @@ class SARL:
     #################################
     # CONSTRUCTOR
 
-    def __init__(self, file_name, min_sup, min_conf, num_parts, num_cuts):
+    def __init__(self, file_name, min_sup, min_conf, num_parts):
         self.trxn_ncoder = TransactionEncoder()
         self.df = self.get_data(file_name)
         self.min_sup = min_sup
         self.min_conf = min_conf
         self.num_parts = num_parts
-        self.num_cuts = num_cuts
-
-    #################################
-    # INTERNAL FUNCTIONS TO MAKE THE LATER STEPS MORE EFFICIENT
-
-
 
     #################################
     # GET DATA AND PRE-PROCESS
@@ -56,37 +47,23 @@ class SARL:
         cat = df
         return cat, num
 
-    def num_to_cat(self, file_name, threshold):
-        df = self.get_cat_and_num_dfs(file_name, threshold)[1]
-        #col_mins = df.min()
-        #col_maxs = df.max()
-        #cols = list(df.columns)
-        #for col in cols:
-        #    df_col = df[col]
-        #    if pd.unique(df_col).shape[0] > threshold:
-        #        df[col + ' length'] = df_col.apply(lambda x: len(x))
-        #        df[col + ' min'] = col_mins[col]
-        #        df[col + ' max'] = col_maxs[col]
-                # maybe delete original cols from df
-                # remember to include threshold
-        return df
-
     def get_data(self, file_name):
-        #df = self.num_to_cat(file_name)
         df = pd.read_csv(file_name, dtype=str, header=None)
         df.fillna('', inplace=True)
         df = df.values.tolist()
-        #df = self.trxn_ncoder.fit(df).transform(df, sparse=True)
-        df = self.trxn_ncoder.fit(df).transform(df)
-        #df = pd.DataFrame.sparse.from_spmatrix(df, columns=self.trxn_ncoder.columns_)
-        df = pd.DataFrame(df, columns=self.trxn_ncoder.columns_)
+        
+        df = self.trxn_ncoder.fit(df).transform(df, sparse=True)
+        #df = self.trxn_ncoder.fit(df).transform(df)
+        
+        df = pd.DataFrame.sparse.from_spmatrix(df, columns=self.trxn_ncoder.columns_)
+        #df = pd.DataFrame(df, columns=self.trxn_ncoder.columns_)
 
         df.drop('', axis=1, inplace=True)
 
         return df
 
     #################################
-    # STEP 1: Find freq 2-itemsets using Apriori (DONE)
+    # STEP 1: Find freq 2-itemsets using Apriori (TO-DO: OPTIMIZE)
 
     def step_1(self):
         freq_itemsets = apriori(self.df, min_support=self.min_sup, max_len=2)
@@ -100,7 +77,6 @@ class SARL:
         freq_itemsets.drop('length', axis=1, inplace=True)
         
         self.freq_itemsets = freq_itemsets
-
 
         return freq_itemsets_2
 
@@ -153,14 +129,6 @@ class SARL:
                 intersect = parts_modified[j].intersection(dataset_modified[i])
                 if len(intersect) > 2:
                     dataset_partitions[j].append(intersect)
-
-        '''
-        for i in dataset_modified:
-            for j in range(self.num_parts):
-                if i.issubset(parts_modified[j]):
-                    dataset_partitions[j].append(list(i))
-                    break
-        '''
         return dataset_partitions
 
     #################################
@@ -170,10 +138,13 @@ class SARL:
         dataset_partitions = self.step_4()
         result = []
         for i in dataset_partitions:
+            
             #trxn_ncoder_arry = self.trxn_ncoder.fit(i).transform(i, sparse=True)
             trxn_ncoder_arry = self.trxn_ncoder.fit(i).transform(i)
+            
             #df = pd.DataFrame.sparse.from_spmatrix(trxn_ncoder_arry, columns=self.trxn_ncoder.columns_)
             df = pd.DataFrame(trxn_ncoder_arry, columns=self.trxn_ncoder.columns_)
+            
             freq_itemsets_trxn_part = fpgrowth(df, min_support=self.min_sup)
             freq_itemsets_trxn_part['length'] = freq_itemsets_trxn_part['itemsets'].apply(lambda x: len(x))
             freq_itemsets_trxn_part = freq_itemsets_trxn_part[freq_itemsets_trxn_part['length'] > 2]
@@ -191,23 +162,17 @@ class SARL:
         for i in result:
             union = pd.concat([union, i], ignore_index=True)
         union = pd.concat([union, self.freq_itemsets], ignore_index=True)
-        #union['length'] = union['itemsets'].apply(lambda x: len(x))
-        #union = union[union['length'] > 2]
-        #union.drop('length', axis=1, inplace=True)
-        
-        #union['support'] = (union['support']).astype(float) / self.df.shape[0]
         return union
 
     #################################
     # STEP 7: Generate association rules using Apriori-ap-genrules on freq itemsets (STEP 6) (DONE)
     def step_7(self):
         union = self.step_6()
-        #rules = association_rules(union, metric='confidence', min_threshold=self.min_conf, support_only=True)
         rules = association_rules(union, metric='confidence', min_threshold=self.min_conf)
         return rules
 
 
-test = SARL(FILE_NAME, MIN_SUP, MIN_CONF, NUM_PARTS, NUM_CUTS)
+test = SARL(FILE_NAME, MIN_SUP, MIN_CONF, NUM_PARTS)
 #start = time.time()
 a = test.step_7()
 #end = time.time()
