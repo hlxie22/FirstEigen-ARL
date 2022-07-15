43c15f9fdcb4e000ecfbe51af076a0c8dda89834
rahul rao <rahul.seth.rao@gmail.com>
2022-07-14 23:07:54 -0500

    cleaned up the code and added a main class

--- DataProcessor.py
+++ DataProcessor.py
@@ -5,12 +5,6 @@ import pandas as pd
 from mlxtend.preprocessing import TransactionEncoder
 
 #################################
-# GLOBAL CONSTS
-
-FILE_NAME = 'TestDataDP.csv'
-#FILE_NAME = 'TestDataSARL.csv'
-
-#################################
 # DATA PROCESSOR CLASS
 
 class DataProcessor:
@@ -30,17 +24,16 @@ class DataProcessor:
 
     def num_to_cat(self, threshold):
         self.num = self.num.astype(str)
-        #self.num.fillna('', inplace=True)
         for col in self.num:
             if pd.unique(self.num[col]).shape[0] > threshold:
                 self.num[f'{col} length'] = self.num[col].apply(len)
                 # add whatever other metadata cols here
                 #self.num.drop[col] # maybe drop the cols with metadata?
 
-    def get_SARL_data(self):
+    def get_SARL_data(self, threshold):
+        self.num_to_cat(threshold)
         trxn_ncoder = TransactionEncoder()
         df = pd.concat([self.cat, self.num], axis=1)
-        print(df)
         #df = pd.concat([self.cat, self.num.set_index(self.cat.index)], axis=1)
 
         df = df.astype(str)
@@ -54,23 +47,3 @@ class DataProcessor:
         df = pd.DataFrame(df, columns=trxn_ncoder.columns_)
 
         return df
-
-a = DataProcessor(FILE_NAME)
-'''
-print('-'*20, 'CAT', '-'*20)
-print(a.cat)
-print('-'*20, 'NUM', '-'*20)
-print(a.num)
-print('-'*20, 'DATE', '-'*20)
-print(a.date)
-
-print('-'*20, 'NUM_TO_CAT', '-'*20)
-a.num_to_cat(4)
-print(a.num)
-'''
-
-a.num_to_cat(4)
-print('-'*20, 'GET_SARL_DATA', '-'*20)
-b = a.get_SARL_data()
-print()
-print(b)
--- SARL.py
+++ SARL.py
@@ -9,15 +9,6 @@ from mlxtend.preprocessing import TransactionEncoder
 from mlxtend.frequent_patterns import association_rules
 import pymetis
 
-
-#################################
-# GLOBAL CONSTS
-
-FILE_NAME = 'TestDataSARL.csv'
-MIN_SUP = 0.1
-MIN_CONF = 0.7
-NUM_PARTS = 2 # k for MLkP
-
 #################################
 # SARL CLASS
 
@@ -26,55 +17,25 @@ class SARL:
     #################################
     # CONSTRUCTOR
 
-    def __init__(self, file_name, min_sup, min_conf, num_parts):
-        self.trxn_ncoder = TransactionEncoder()
-        self.df = self.get_data(file_name)
+    def __init__(self, df, min_sup, min_conf, num_parts):
+        self.df = df
         self.min_sup = min_sup
         self.min_conf = min_conf
         self.num_parts = num_parts
 
     #################################
-    # GET DATA AND PRE-PROCESS
-
-    def get_cat_and_num_dfs(self, file_name, threshold):
-        df = pd.read_csv(file_name, dtype=str)
-        df.fillna('', inplace=True)
-        num = pd.DataFrame()
-        for col in df:
-            df_col = df[col]
-            if pd.unique(df_col).shape[0] > threshold:
-                num[col] = df.pop(col)
-        cat = df
-        return cat, num
-
-    def get_data(self, file_name):
-        df = pd.read_csv(file_name, dtype=str, header=None)
-        df.fillna('', inplace=True)
-        df = df.values.tolist()
-        
-        df = self.trxn_ncoder.fit(df).transform(df, sparse=True)
-        #df = self.trxn_ncoder.fit(df).transform(df)
-        
-        df = pd.DataFrame.sparse.from_spmatrix(df, columns=self.trxn_ncoder.columns_)
-        #df = pd.DataFrame(df, columns=self.trxn_ncoder.columns_)
-
-        df.drop('', axis=1, inplace=True)
-
-        return df
-
-    #################################
     # STEP 1: Find freq 2-itemsets using Apriori (TO-DO: OPTIMIZE)
 
     def step_1(self):
-        freq_itemsets = apriori(self.df, min_support=self.min_sup, max_len=2)
-        freq_itemsets['support'] = (freq_itemsets['support'] * self.df.shape[0]).astype(int)
-        freq_itemsets['length'] = freq_itemsets['itemsets'].apply(len)
+        freq_itemsets_1_2 = apriori(self.df, min_support=self.min_sup, max_len=2)
+        freq_itemsets_1_2['support'] = (freq_itemsets_1_2['support'] * self.df.shape[0]).astype(int)
+        freq_itemsets_1_2['length'] = freq_itemsets_1_2['itemsets'].apply(len)
 
-        freq_itemsets_2 = freq_itemsets[freq_itemsets['length'] == 2]
+        freq_itemsets_2 = freq_itemsets_1_2[freq_itemsets_1_2['length'] == 2]
         freq_itemsets_2.drop('length', axis=1, inplace=True)
 
-        freq_itemsets.drop('length', axis=1, inplace=True)
-        self.freq_itemsets = freq_itemsets
+        freq_itemsets_1_2.drop('length', axis=1, inplace=True)
+        self.freq_itemsets_1_2 = freq_itemsets_1_2
 
         return freq_itemsets_2
 
@@ -82,13 +43,14 @@ class SARL:
     # STEP 2: Construct IAG (DONE)
 
     def step_2(self):
-        freq_itemsets = self.step_1()
-        nodes = pd.DataFrame(freq_itemsets.pop('itemsets'))
+        freq_itemsets_2 = self.step_1()
+        nodes = pd.DataFrame(freq_itemsets_2.pop('itemsets'))
         nodes[[0, 1]] = pd.DataFrame(nodes['itemsets'].tolist(), index=nodes.index)
         nodes.drop('itemsets', axis=1, inplace=True)
         nodes = nodes.values
-        freq_itemsets = freq_itemsets.values
+        edge_weights = freq_itemsets_2.values
         unique_nodes = np.unique(nodes)
+
         adjncy = []
         xadj = [0]
         eweights = []
@@ -98,7 +60,7 @@ class SARL:
             where = where.T
             for row, col in where:
                 adjncy.append(nodes[row][col]) # maybe change index?
-                eweights.append(freq_itemsets[row][0])
+                eweights.append(edge_weights[row][0])
             xadj.append(xadj[-1] + where.shape[0])
         return adjncy, xadj, eweights
 
@@ -134,15 +96,16 @@ class SARL:
 
     def step_5(self):
         dataset_partitions = self.step_4()
+        trxn_ncoder = TransactionEncoder()
         result = []
         for i in dataset_partitions:
             
-            #trxn_ncoder_arry = self.trxn_ncoder.fit(i).transform(i, sparse=True)
-            trxn_ncoder_arry = self.trxn_ncoder.fit(i).transform(i)
+            #trxn_ncoder_arry = trxn_ncoder.fit(i).transform(i, sparse=True)
+            trxn_ncoder_arry = trxn_ncoder.fit(i).transform(i)
             
-            #df = pd.DataFrame.sparse.from_spmatrix(trxn_ncoder_arry, columns=self.trxn_ncoder.columns_)
-            df = pd.DataFrame(trxn_ncoder_arry, columns=self.trxn_ncoder.columns_)
-            
+            #df = pd.DataFrame.sparse.from_spmatrix(trxn_ncoder_arry, columns=trxn_ncoder.columns_)
+            df = pd.DataFrame(trxn_ncoder_arry, columns=trxn_ncoder.columns_)
+
             freq_itemsets_trxn_part = fpgrowth(df, min_support=self.min_sup)
             freq_itemsets_trxn_part['length'] = freq_itemsets_trxn_part['itemsets'].apply(len)
             freq_itemsets_trxn_part = freq_itemsets_trxn_part[freq_itemsets_trxn_part['length'] > 2]
@@ -159,7 +122,7 @@ class SARL:
         union = pd.DataFrame()
         for i in result:
             union = pd.concat([union, i], ignore_index=True)
-        union = pd.concat([union, self.freq_itemsets], ignore_index=True)
+        union = pd.concat([union, self.freq_itemsets_1_2], ignore_index=True)
         return union
 
     #################################
@@ -168,23 +131,3 @@ class SARL:
         union = self.step_6()
         rules = association_rules(union, metric='confidence', min_threshold=self.min_conf)
         return rules
-
-
-test = SARL(FILE_NAME, MIN_SUP, MIN_CONF, NUM_PARTS)
-#start = time.time()
-a = test.step_7()
-#end = time.time()
-#print(end-start)
-print(a)
-
-'''
-start = time.time()
-df = pd.read_csv(FILE_NAME, dtype=str)
-for col in df:
-    df[col] = df[col].map({col: True})
-df.fillna(False, inplace=True)
-a = fpgrowth(df, min_support=MIN_SUP)
-end = time.time()
-print(a)
-print(end-start)
-'''
